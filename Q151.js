32d454fb20666349d6336e3ee87114303b40506f
Anshu Shee <anshu.shee.cg@gmail.com>
2025-12-22 10:09:51 +0530

    Updated file

--- Q151.js
+++ Q151.js
@@ -1,10 +1,10 @@
-let str = "Anshu Shee hola";
+let str = "Anshu Shee hola   am";
 
 let words = str.split(" ");
 let minLength = words[0].length;
 
 for (let i = 1; i < words.length; i++) {
-    if (words[i].length < minLength) {
+    if (words[i].length < minLength && words[i]!=0) {
         minLength = words[i].length;
     }
 }
