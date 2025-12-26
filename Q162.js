f5cc73b07380580a5403b3f98f5c7e756dfba721
Anshu Shee <anshu.shee.cg@gmail.com>
2025-12-26 13:42:25 +0530

    updated files

---
+++ Q162.js
@@ -0,0 +1,14 @@
+// let num = [1,2,3];
+// let arr = []
+// for(let i=0; i<num.length; i++){
+//     arr.push(num[i]*3)
+// }
+//     console.log(arr)
+
+    function triple(){
+        return arr.map(x => x*3);
+    }
+    let arr =[1,2,3];
+    console.log(triple(arr))
+
+  
