f964d6feea55732f3c2bcd2002b196f868a94b72
Anshu Shee <anshu.shee.cg@gmail.com>
2025-12-26 13:54:43 +0530

    updates files

--- Q163.js
+++ Q163.js
@@ -1,4 +1,4 @@
-let arr = [-1,2,-3];
+// let arr = [-1,2,-3];
 // let arr1 =[];
 // for(let i=0;i<arr.length;i++){
 //     if(arr[i]<0){
@@ -10,3 +10,11 @@ let arr = [-1,2,-3];
 //     }
 // }
 // console.log(arr1)
+
+function absolute(num){
+for(let i=0;i<num.length;i++){
+    num[i]=Math.abs(num[i]);
+}
+console.log(num)
+}
+absolute([-1,-2,1,3]);
