8b0ec73508347a47e082eb770f0b0cd5a33e9b09
Anshu Shee <anshu.shee.cg@gmail.com>
2025-12-31 09:38:59 +0530

    Updated files

--- Q187.js
+++ Q187.js
@@ -1,6 +1,21 @@
-let arr = [1,-5,2,4,3,7];
-arr.sort((a,b) => a-b);
+// let arr = [1,-5,2,4,3,7];
+// arr.sort((a,b) => a-b);
+// console.log(arr);
+
+
+function selectionSort(arr){
+
+for(let i=0;i<arr.length; i++){
+let max =- Infinity;
+let maxIndex=0;
+for(let j=0;j<arr.length-i;j++){
+if(arr[j]>max){
+max=arr[j];
+maxIndex=j;
+}
+}
+[arr[arr.length-i-1],arr[maxIndex]]=[arr[maxIndex],arr[arr.length-i-1]];
+}
 console.log(arr);
-
-let input = [1,2,3,4,5]
-
+}
+selectionSort([3, 1, 4, 2]);
